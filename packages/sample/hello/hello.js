39b4ccb811e7f5eea909ba3ec363da208ce69706
Ahmed Butt <86029001+abuttDO@users.noreply.github.com>
2023-11-14 14:26:20 +0500

    Update hello.js

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -2,7 +2,7 @@ function main(args) {
     let name = args.name || 'stranger'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
-    return {"body": greeting}
+    return {"body": greet}
   }
 
 exports.main = main
